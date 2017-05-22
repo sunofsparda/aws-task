16766d67bf8d552b334295a34a98446862093c24
Anton Cherlyonok <sunofsparda@gmail.com>
2017-05-22 19:00:49 +0300

    added terraform stack

---
+++ stack.tf
@@ -0,0 +1,252 @@
+/* A multi
+   line comment. */
+/*
+# EXAMPLES
+# https://github.com/hashicorp/terraform/tree/master/examples
+# https://github.com/hashicorp/terraform/tree/master/examples/aws-two-tier
+*/
+
+# Specify the provider and access details
+provider "aws" {
+  region = "${var.aws_region}"
+
+}
+
+# Create a VPC to launch our instances into
+resource "aws_vpc" "default" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = false
+}
+
+# Create an internet gateway to give our subnet access to the outside world
+resource "aws_internet_gateway" "default" {
+  vpc_id = "${aws_vpc.default.id}"
+
+  tags {
+    Name = "default"
+  }
+}
+
+# Create a private subnet to launch our instances into
+resource "aws_subnet" "private" {
+  vpc_id = "${aws_vpc.default.id}"
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = false
+
+}
+# Create a public subnet
+resource "aws_subnet" "public" {
+  vpc_id = "${aws_vpc.default.id}"
+  cidr_block = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+}
+
+# NAT Gateway
+resource "aws_nat_gateway" "default" {
+  allocation_id = "${aws_eip.default.id}"
+  subnet_id = "${aws_subnet.public.id}"
+  depends_on = [
+    "aws_internet_gateway.default",
+    "aws_eip.default"]
+}
+
+# EIP
+resource "aws_eip" "default" {
+  vpc = true
+}
+
+# RouteTablePrivate
+resource "aws_route_table" "private" {
+  vpc_id = "${aws_vpc.default.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_nat_gateway.default.id}"
+  }
+}
+# RouteTablePublic
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.default.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.default.id}"
+  }
+}
+//# RoutePrivate
+//resource "aws_route" "private" {
+//  route_table_id = "${aws_vpc.default.default_route_table_id}"
+//  destination_cidr_block = "0.0.0.0/0"
+//  gateway_id = "${aws_nat_gateway.default.id}"
+//}
+//# RoutePublic
+//resource "aws_route" "public" {
+//  route_table_id = "${aws_vpc.default.default_route_table_id}"
+//  destination_cidr_block = "0.0.0.0/0"
+//  gateway_id = "${aws_internet_gateway.default.id}"
+//}
+
+# SubnetRouteTableAssociationPrivate
+resource "aws_route_table_association" "public" {
+  subnet_id = "${aws_subnet.public.id}"
+  route_table_id = "${aws_route_table.public.id}"
+}
+# SubnetRouteTableAssociationPublic
+resource "aws_route_table_association" "private" {
+  subnet_id = "${aws_subnet.private.id}"
+  route_table_id = "${aws_route_table.private.id}"
+}
+# SecurityGroups
+
+# SecurityGroupBackend
+resource "aws_security_group" "backend" {
+  name = "backend_http_ssh"
+  description = "Allow http and ssh inbound traffic"
+  vpc_id = "${aws_vpc.default.id}"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+# SecurityGroupBastion
+resource "aws_security_group" "bastion" {
+  name = "bastion_ssh"
+  description = "Allow ssh inbound traffic"
+  vpc_id = "${aws_vpc.default.id}"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [
+      "${var.aws_security_group_cidr_blocks_epminscope}"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "bastion_ssh"
+  }
+}
+# SecurityGroupELB
+resource "aws_security_group" "elb" {
+  name = "elbaccess_epam_minsk"
+  description = "Allow http inbound traffic from epam-minsk"
+  vpc_id = "${aws_vpc.default.id}"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = [
+      "${var.aws_security_group_cidr_blocks_epminscope}"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+# Ec2Instances
+resource "aws_instance" "web" {
+  depends_on = ["aws_route_table.private"]
+  ami = "${var.aws_amis["us-east-1"]}"
+  count = 2
+  source_dest_check = true
+  instance_type = "${var.aws_instance_types}"
+  vpc_security_group_ids = ["${aws_security_group.backend.id}"]
+  subnet_id = "${aws_subnet.private.id}"
+
+  key_name = "${var.key_name}"
+  user_data = "${file("provision.sh")}"
+
+  connection {
+    user = "root"
+  }
+}
+
+resource "aws_instance" "bastion" {
+  depends_on = ["aws_route_table.private"]
+  ami = "${var.aws_amis["us-east-1"]}"
+  count = 1
+  source_dest_check = false
+  instance_type = "${var.aws_instance_types}"
+  vpc_security_group_ids = ["${aws_security_group.bastion.id}"]
+  subnet_id = "${aws_subnet.public.id}"
+  key_name = "${var.key_name}"
+
+  connection {
+    user = "root"
+  }
+}
+
+# ElasticLoadBalancer
+resource "aws_elb" "elb" {
+  name               = "stack-elb"
+//  availability_zones = "${var.availability_zones["us-east-1"]}"
+  security_groups = ["${aws_security_group.elb.id}"]
+  subnets = ["${aws_subnet.public.id}"]
+
+//  access_logs {
+//    bucket        = "foo"
+//    bucket_prefix = "bar"
+//    interval      = 60
+//  }
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  listener {
+    instance_port      = 80
+    instance_protocol  = "http"
+    lb_port            = 80
+    lb_protocol        = "http"
+//    ssl_certificate_id = "arn:aws:iam::123456789012:server-certificate/certName"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 10
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 5
+  }
+
+  instances                   = ["${aws_instance.web.*.id}"]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 30
+  connection_draining         = true
+  connection_draining_timeout = 30
+
+  tags {
+    Name = "stack-elb"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,98 @@
+# EXAMPLE
+
+//variable "aws_access_key" {}
+//variable "aws_secret_key" {}
+//variable "aws_key_path" {}
+//variable "aws_key_name" {}
+//
+//variable "aws_region" {
+//  default = "us-west-2"
+//}
+//
+//variable "network" {
+//  default = "10.10"
+//}
+//
+//variable "offset" {
+//  default = ""
+//}
+//
+//variable "aws_vpc_id" {}
+//variable "aws_internet_gateway_id" {}
+//variable "aws_route_table_public_id" {}
+//variable "aws_route_table_private_id" {}
+//variable "aws_subnet_cfruntime-2a_availability_zone" {
+//  default = "us-west-2a"
+//}
+//variable "aws_subnet_cfruntime-2b_availability_zone" {
+//  default = "us-west-2a"
+//}
+//
+//variable "aws_ubuntu_ami" {
+//  type = "map"
+//  default = {
+//    us-east-1 = "ami-98aa1cf0"
+//    us-west-1 = "ami-736e6536"
+//    us-west-2 = "ami-37501207"
+//    ap-northeast-1 = "ami-df4b60de"
+//    ap-southeast-1 = "ami-2ce7c07e"
+//    ap-southeast-2 = "ami-1f117325"
+//    eu-west-1 = "ami-f6b11181"
+//    sa-east-1 = "ami-71d2676c"
+//  }
+//}
+//
+//variable "aws_centos_ami" {
+//  type = "map"
+//  default = {
+//    us-east-1 = "ami-00a11e68"
+//    us-west-1 = "ami-e04b7aa5"
+//    us-west-2 = "ami-3425be04"
+//    ap-northeast-1 = "ami-9392dc92"
+//    ap-southeast-1 = "ami-dcbeed8e"
+//    ap-southeast-2 = "ami-89e88db3"
+//    eu-west-1 = "ami-af6faad8"
+//    sa-east-1 = "ami-73ee416e"
+//  }
+//}
+
+variable "aws_region" {
+}
+
+variable "key_name" {
+  description = "AWS key"
+}
+
+variable "key_path" {
+  description = "Key path for SSHing into EC2"
+}
+
+# Amazon Linux AMI 2017.03.0
+variable "aws_amis" {
+  type = "map"
+  default = {
+    us-east-1 = "ami-c58c1dd3"
+  }
+}
+
+variable "aws_instance_types" {
+  default = "t2.micro"
+}
+
+variable "aws_security_group_cidr_blocks_epminscope" {
+  type = "list"
+  default = [
+    "213.184.243.0/24",
+    "217.21.56.0/24",
+    "217.21.63.0/24",
+    "213.184.231.0/24",
+    "86.57.255.88/29"]
+}
+
+variable "availability_zones" {
+  description = "ELB availability zones"
+  type = "map"
+  default = {
+    us-east-1 = ["us-east-1a","us-east-1a","us-east-1b","us-east-1c","us-east-1d"]
+  }
+}
